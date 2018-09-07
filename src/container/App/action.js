3409664d30e9442f88705cf97a507bd46292c2f9
Suraj Kiran S <suraj.kiran@aot-technologies.com>
2018-09-07 17:46:03 +0530

    beautify

--- src/container/App/action.js
+++ src/container/App/action.js
@@ -1,18 +1,18 @@
 export const simpleAction = () => ({
-    type: 'SIMPLE_ACTION',
-    payload: 'result_of_simple_action'
+  type: 'SIMPLE_ACTION',
+  payload: 'result_of_simple_action'
 })
 
 export const addNotebook = () => ({
-    type: 'ADD_NOTEBOOK',
-    payload: 'result_of_simple_action'
+  type: 'ADD_NOTEBOOK',
+  payload: 'result_of_simple_action'
 })
 
 export const testApi = () => ({
-    type: 'LOAD',
-    payload: {
-      request:{
-        url:'/todos/1'
-      }
+  type: 'LOAD',
+  payload: {
+    request: {
+      url: '/todos/1'
     }
-  })
+  }
+})
